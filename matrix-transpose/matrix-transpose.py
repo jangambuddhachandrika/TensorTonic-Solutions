75cab35737a1e4bd46c75dd8ae098019f1e35f4c
Jangam Buddha Chandrika <98312672+jangambuddhachandrika@users.noreply.github.com>
2026-08-17 14:57:35 +0530

    Solve matrix-transpose

---
+++ matrix-transpose/matrix-transpose.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def matrix_transpose(A):
+    """
+    Return the transpose of matrix A (swap rows and columns).
+    """
+    # Write code here
+    m = len(A)
+    n = len(A[0])
+
+    result = np.zeros((n, m))
+
+    for i in range(m):
+        for j in range(n):
+            result[j][i] = A[i][j]
+
+    return result
+    pass
